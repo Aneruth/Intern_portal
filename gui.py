d4f8ce49eab43f6c8ba5725c4b2ee9e50124e30b
Aneruth Mohanasundaram <ane1998@gmail.com>
2020-12-03 21:56:46 +0530

    temp changes for the score value

--- gui.py
+++ gui.py
@@ -1,8 +1,6 @@
-# import tkinter as tk
 import pandas as pd
 from tkinter import *
-# from tkinter import tk
-# from tkinter.ttk import *
+from random import *
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
@@ -22,9 +20,9 @@ Label(root, text="University",fg='red').grid(row=2, column = 0)
 e3 = Entry(root)
 e3.grid(row=2, column = 1)
 
-Label(root, text="Score",fg='red').grid(row=3, column = 0)
-e4 = Entry(root)
-e4.grid(row=3, column = 1)
+# Label(root, text="Score",fg='red').grid(row=3, column = 0)
+# e4 = Entry(root)
+# e4.grid(row=3, column = 1)
 
 ''''
 Machine Learning Model
@@ -33,15 +31,13 @@ df = pd.read_csv('/Users/aneruthmohanasundaram/Documents/VUB/1/Advanced Programm
 df.head()
 # Removing the unwanted column in dataset
 df = df.drop('Serial No.',axis=1)
-    
+
 # Performing Train test split method to train and test the data 
-''''
-test_size --> Spliting the test size as 60:40 ratio for train and test of our given dataset.abs
-random_state --> It'll decide the splitting of data into train and test indices in our case.(The number can be randomly described.)
-'''''
 X = df[['University Score', 'Round 1', 'Round 2 ', 'Research','Paper Presented']]
 y= df['Chance of Admit ']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+# test_size --> Spliting the test size as 60:40 ratio for train and test of our given dataset.abs
+# random_state --> It'll decide the splitting of data into train and test indices in our case.(The number can be randomly described.)
 
 # Intialising the model for our project
 lr = LinearRegression()
@@ -52,54 +48,55 @@ fit = lr.fit(X_train,y_train)
 # Model Predicting
 pre = lr.predict(X_test)
 
-''''
-Machine Learning Model Ends here
-'''''
-
 #Submit button
 def callback(): # Fuction for getting callback from user input. 
     print("Student Name:", e1.get())
     print("Department:", e2.get())
     print("University:", e3.get())
-    print('Score:',e4.get())
-    # chance = [[3.5,3.5,3.5,1,4]] # User Manually enters the input for a student, it may change to hardcode values 
-    # if (abs(chance[0][0])<=5) and (abs(chance[0][1])<=5) and (abs(chance[0][2])<=5): # Won't accept negative value as input
-    #     if chance[0][3]<=1:
-    #         pre = lr.predict(chance) # Predicting the chance of acceptance
-    #         for i in pre: # Iterating through loop, which comes out of a list
-    #             if (i*100).round(2) < 70: # To check if the candidate secured more than 70 
-    #                 print('Candiate not Selected')
-    #             elif (i*100).round(2) > 100:
-    #                 print('You are over qualified.')
-    #             else:
-    #                 print(f'You are selected with {(i*100).round(2)}%')
-    #     else:
-    #         print('Wrong Input')
-    # else:
-    #     print('Wrong Input')
-    # print ("Ah shit here we go again!!")
+    # print('Score:',e4.get())
+    a = randint(0,5)
+    b = randint(0,5)
+    c = randint(0,5)
+    d = randint(0,1)
+    e = randint(0,15)
+    values = [[a,b,c,d,e]]
+    if (abs(values[0][0])<=5) and (abs(values[0][1])<=5) and (abs(values[0][2])<=5): # Won't accept negative value as input
+        if values[0][3]<=1:
+            pre = lr.predict(values) # Predicting the chance of acceptance
+            for i in pre: # Iterating through loop, which comes out of a list
+                if (i*100).round(2) < 70: # To check if the candidate secured more than 70 
+                    print('Candiate not Selected')
+                elif (i*100).round(2) > 100:
+                    print('You are over qualified.')
+                else:
+                    print(f'You are selected with {(i*100).round(2)}%')
+        else:
+            print('Wrong Input')
+    else:
+        print('Wrong Input')
+    print('\n') # <---- To sepearate the students 
 
 # Reset feature after inout entered 
 def res():
     e1.delete(0,END)
     e2.delete(0,END)
     e3.delete(0,END)
-    e4.delete(0,END)
+    # e4.delete(0,END)
 
 
 # Close button 
 def close_window(): 
     root.destroy()
 
-command= lambda: [f() for f in [callback, close_window]]
+command= lambda: [f() for f in [callback, res]]
 
 # Submit button
 MyButton1 = Button(root, text="Submit", width=10, command=command)
-MyButton1.grid(row=16, column=1)
+MyButton1.grid(row=16, column=0)
 
 
-# # Quit Button
-# button = Button (root, text = "Good-bye.", command = close_window)
-# button.grid(row=16, column=2)
+# Quit Button
+button = Button (root, text = "Good-bye.", command = close_window)
+button.grid(row=16, column=2)
 
 root.mainloop()
